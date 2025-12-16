2b95f5d95890a79ee578141b2a73df727d59a4cd
Slaviaaa2 <maroro9452@gmail.com>
2025-12-16 20:02:04 +0900

    私はもうこの問題には対処しない！！！

--- Slafight_Plugin_EXILED/Extensions/CassieExtensions.cs
+++ Slafight_Plugin_EXILED/Extensions/CassieExtensions.cs
@@ -9,14 +9,9 @@ public static class CassieExtensions
 {
     public static void CassieTranslated(string words, string translated, bool isBell)
     {
-        float waitTime = translated.Length / 8f;
-        if (waitTime <= 0f) waitTime = 1f;
-        Timing.CallDelayed(0f, () =>
+        foreach (Player player in Player.List)
         {
-            foreach (Player player in Player.List)
-            {
-                player.MessageTranslated(words,String.Empty,translated,isBell,false);
-            }
-        });
+            player.MessageTranslated(words,String.Empty,translated,isBell,false);
+        }
     }
 }
