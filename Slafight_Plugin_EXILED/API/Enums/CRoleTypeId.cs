87f97bac5cd5338ef9d32b41e5b2d13c3cd166eb
Slaviaaa2 <maroro9452@gmail.com>
2025-12-22 15:31:45 +0900

    Update CRoleTypeId enum with new role types

--- Slafight_Plugin_EXILED/API/Enums/CRoleTypeId.cs
+++ Slafight_Plugin_EXILED/API/Enums/CRoleTypeId.cs
@@ -9,6 +9,15 @@ public enum CRoleTypeId
     None = 0,
     Scp096Anger = 1,
     Scp3005 = 2,
-    FifthistRescure = 3,
-    ChaosCommando = 4
+    Scp966 = 3,
+    FifthistRescure = 4,
+    FifthistPriest = 5,
+    ChaosCommando = 6,
+    NtfLieutenant = 7,
+    HdInfantry = 8,
+    HdCommander = 9,
+    EvacuationGuard = 10,
+    ZoneManager = 11,
+    FacilityManager = 12,
+    FifthistConvert = 13
 }
