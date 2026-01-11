58cab1323faf003678909181444187f26f2b9e53
Slaviaaa2 <maroro9452@gmail.com>
2026-01-11 12:29:10 +0900

    Change keybind for proximity chat from LeftAlt to V

--- Slafight_Plugin_EXILED/ServerSpecifics.cs
+++ Slafight_Plugin_EXILED/ServerSpecifics.cs
@@ -13,7 +13,7 @@ public class ServerSpecifics
         List<ServerSpecificSettingBase> SettingsList = new List<ServerSpecificSettingBase>()
         {
             new SSGroupHeader(0,"シャープ鯖"),
-            new SSKeybindSetting(1,"近接チャット",KeyCode.LeftAlt,true,false,hint:"一部の利用可能ロールで、近接チャットを使用するのに必要です。左Altを推奨します"),
+            new SSKeybindSetting(1,"近接チャット",KeyCode.V,true,false,hint:"一部の利用可能ロールで、近接チャットを使用するのに必要です。左Altを推奨します"),
             new SSPlaintextSetting(2,"キャラクター名","",20,hint:"RPのキャラ名です。設定した名前の後に本当の名前が表示されます。"),
             new SSKeybindSetting(3,"アビリティ使用",KeyCode.LeftAlt,true,false,hint:"一部の利用可能ロールで、アビリティを使用するのに必要です。左Altを推奨します"),
             new SSKeybindSetting(4,"アビリティ切り替え",KeyCode.Mouse2,true,false,hint:"複数アビリティを所持している際の切り替えボタンです。中マウスボタンを推奨します")
