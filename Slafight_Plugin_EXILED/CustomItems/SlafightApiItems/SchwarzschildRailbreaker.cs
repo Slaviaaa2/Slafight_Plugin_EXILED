68002e6f2def194c36f5eed943bca810d69dcec2
Slaviaaa2 <maroro9452@gmail.com>
2026-04-29 12:52:03 +0900

    feat: SchwarzschildRailbreaker

--- Slafight_Plugin_EXILED/CustomItems/SlafightApiItems/SchwarzschildRailbreaker.cs
+++ Slafight_Plugin_EXILED/CustomItems/SlafightApiItems/SchwarzschildRailbreaker.cs
@@ -1,6 +1,15 @@
+#nullable enable
+using System.Collections.Generic;
+using Slafight_Plugin_EXILED.API.Features;
+
 namespace Slafight_Plugin_EXILED.CustomItems.SlafightApiItems;
 
-public class SchwarzschildRailbreaker
+public class SchwarzschildRailbreaker : CItemHybrid
 {
-    
+    public override string DisplayName => "シュバルツシルト・レイルブレイカー";
+    public override string Description => "クエィサァーとレールガンを切り替えられる複合武器";
+    protected override string UniqueKey => "SchwarzschildRailbreaker";
+
+    protected override List<CItem> BuildSubModes()
+        => [new SchwarzschildQuasar(), new GunGoCRailgunFull()];
 }
